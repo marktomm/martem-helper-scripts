e40e03bc02f09e88445d82091a8afe2dbea7b0da
Mark Tomm <mark.tomm@martem.ee>
2017-09-05 23:36:25 +0300

    telem-app
    + script to transform config variables from cpp file into mediawiki table format

---
+++ telem-app/get_setting_desc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# inteded input file is gw/Gateway6/source/Core2/System/settingdescriptions.cpp
+# Transforms into mediawiki three column table format: argument, default value, description
+
+set -eo pipefail
+
+main() {
+    local source_file=/home/mark/Development/telem-cloner/next/gw/Gateway6/source/Core2/System/settingdescriptions.cpp
+    
+    grep "^[\t ]*(\"[a-z]" "${source_file}" | 
+        awk 'BEGIN { FS = "," } ;  
+            /default_value/{ printf $1 " " $2 " " $3  "\n" } ;
+            !/default_value/{ printf $1 " " $2 "\n" }' |
+        sed 's/^[\t ]*("\([^"]*\)".*default_value([\t ]*"\?\([^"]*\)"\?[\t ]*)[\t ]*"\([^"]*\)"\?.*/|-\n| \1 \n| \2 \n| \3/g' |
+        sed '/default_value/!s/^[ \t]*("\([^"]*\)".*"\([^"]*\)".*/|-\n| \1 \n| -\n| \2/g' |
+        sed '/default_value/!s/^[ \t]*("\([^"]*\)"[\t ]*\(value.*()\)/|-\n| \1 \n| -\n| \2/g' |
+        sed '/timezone.default/d'
+        
+    grep "^[\t ]*(\"[a-z]" "${source_file}" | grep -F 'timezone.default' | 
+        sed 's/^[\t ]*("\([^"]*\)".*default_value([\t ]*"\?\([^"]*\)"),[\t ]*"\([^"]*\)"\?.*/|-\n| \1 \n| \2 \n| \3/g'
+}
+
+main
