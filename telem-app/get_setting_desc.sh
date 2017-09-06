352be5c8a04e53ee1aa99b9973f46eeedb2a1b69
Mark Tomm <mark.tomm@martem.ee>
2017-09-06 21:06:45 +0300

    telem-app
    + script rewrite

--- telem-app/get_setting_desc.sh
+++ telem-app/get_setting_desc.sh
@@ -7,18 +7,35 @@ set -eo pipefail
 
 main() {
     local source_file=/home/mark/Development/telem-cloner/next/gw/Gateway6/source/Core2/System/settingdescriptions.cpp
-    
-    grep "^[\t ]*(\"[a-z]" "${source_file}" | 
-        awk 'BEGIN { FS = "," } ;  
-            /default_value/{ printf $1 " " $2 " " $3  "\n" } ;
-            !/default_value/{ printf $1 " " $2 "\n" }' |
-        sed 's/^[\t ]*("\([^"]*\)".*default_value([\t ]*"\?\([^"]*\)"\?[\t ]*)[\t ]*"\([^"]*\)"\?.*/|-\n| \1 \n| \2 \n| \3/g' |
-        sed '/default_value/!s/^[ \t]*("\([^"]*\)".*"\([^"]*\)".*/|-\n| \1 \n| -\n| \2/g' |
-        sed '/default_value/!s/^[ \t]*("\([^"]*\)"[\t ]*\(value.*()\)/|-\n| \1 \n| -\n| \2/g' |
-        sed '/timezone.default/d'
         
-    grep "^[\t ]*(\"[a-z]" "${source_file}" | grep -F 'timezone.default' | 
-        sed 's/^[\t ]*("\([^"]*\)".*default_value([\t ]*"\?\([^"]*\)"),[\t ]*"\([^"]*\)"\?.*/|-\n| \1 \n| \2 \n| \3/g'
+    while read line; do
+        [[ ${line} =~ \(\"[a-z] ]] && {
+            line=$(echo ${line} | 
+                tr -s " " |             # squeeze whitespace
+                sed 's/^[\t ]*//')      # remove any whitespace from start
+            
+            [[ ${line} =~ value\<[_\ a-zA-Z0-9:]*\>\(\) ]] && {
+                local argument=$(echo ${line%% *} | sed 's/^("\([-a-zA-Z.]*\).*$/\1/')
+                local description=$(echo ${line##* \"} | sed 's/\(^[- \ta-zA-Z0-9*.,:;/\()!?_]*\)"[\t ]\?)[\t ]\?$/\1/')
+                local pseudo_default=$(echo ${line#*, } | sed 's/\(^value.*\)), .*$/\1/')
+                [[ ${pseudo_default} =~ default_value ]] && {
+                    local default=$(echo ${pseudo_default#*default_value\(} | sed 's/^"\?\(.*\)"[\t ]\?$/\1/')
+                } || {
+                    local default=$(echo ${pseudo_default} | sed 's/^value<[\t ]\?\([_a-zA-Z0-9:]*\)[\t ]\?>.*$/\1/')
+                }
+            } || {
+                local argument=$(echo ${line} | sed 's/("\([-a-zA-Z0-9.]*\)".*$/\1/')
+                local description=$(echo ${line} | sed 's/^.*, "\([- \ta-zA-Z0-9*.,:;/\()!?_]*\)").*$/\1/')
+                local default="N/A"
+            }
+            echo '|-'
+            echo "| ${argument}"
+            echo "| ${default}"
+            echo "| ${description}"
+            # http://www.thegeekstuff.com/2010/07/bash-string-manipulation
+            # https://www.networkworld.com/article/2693361/unix-tip-using-bash-s-regular-expressions.html
+        }
+    done < ${source_file}
 }
 
 main
